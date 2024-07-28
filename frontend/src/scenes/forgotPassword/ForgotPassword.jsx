198aa4b2b14c0fff829c8a78f8036f08a4292ec1
maxim2100 <maxsim2100@gmail.com>
2024-07-28 22:38:03 +0300

    Update and fixes bugs

--- frontend/src/scenes/forgotPassword/ForgotPassword.jsx
+++ frontend/src/scenes/forgotPassword/ForgotPassword.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography, useTheme } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const { palette } = useTheme();
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -32,10 +35,36 @@ const ForgotPassword = () => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
+          fullWidth
         />
-        <Button type="submit" variant="contained" color="primary">Send Reset Link</Button>
+        {message && <Typography>{message}</Typography>}
+        <Button
+          fullWidth
+          type="submit"
+          sx={{
+            m: "2rem 0",
+            p: "1rem",
+            backgroundColor: palette.primary.main,
+            color: palette.background.alt,
+            "&:hover": { color: palette.primary.main },
+          }}
+        >
+          Send Reset Link
+        </Button>
+        <Typography
+          onClick={() => navigate("/login")}
+          sx={{
+            textDecoration: "underline",
+            color: palette.primary.main,
+            "&:hover": {
+              cursor: "pointer",
+              color: palette.primary.light,
+            },
+          }}
+        >
+          Sign Up here.
+        </Typography>
       </form>
-      {message && <Typography>{message}</Typography>}
     </Box>
   );
 };
