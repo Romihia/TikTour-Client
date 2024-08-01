c2c2a2642ef9f688e274c23ebae81a2df4340034
darf10 <118679710+darf10@users.noreply.github.com>
2024-08-01 04:23:07 +0300

    Profile Page DONE: fixed follow/unfollow - works added delete account - works NOT DONE: added change password - does not work yet edit profile page needs to be added

--- frontend/src/components/Following.jsx
+++ frontend/src/components/Following.jsx
@@ -41,7 +41,6 @@ const Following = ({ userId, name, subtitle, userPicturePath }) => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-
       const data = await response.json();
       console.log('Response data:', data);
 
