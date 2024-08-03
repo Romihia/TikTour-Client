dc6f9225b691595b21decc1acbb3f9fc5745f237
Romihia <118122586+Romihia@users.noreply.github.com>
2024-08-03 19:24:13 +0300

    Add 'Remember Me' functionality to login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
@@ -9,15 +10,45 @@ import ResetPassword from "scenes/resetPassword";
 import VerifyEmail from './scenes/verifyUser/VerifyEmail';
 import TermsAndConditions from './scenes/termsPage';
 import { useMemo } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode"; 
+import { setLogout } from "./state";
 
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  const isAuth = Boolean(useSelector((state) => state.token));
+  const token = useSelector((state) => state.token);
+  const dispatch = useDispatch();
+
+  const checkTokenValidity = () => {
+    const token = Cookies.get("token");
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token); 
+        const currentTime = Date.now() / 1000; 
+
+        if (decodedToken.exp < currentTime) {
+          Cookies.remove("token");
+          dispatch(setLogout()); 
+          console.log("Token expired. Logging out.");
+        }
+      } catch (error) {
+        Cookies.remove("token");
+        dispatch(setLogout());
+        console.log("Token decoding failed. Logging out.");
+      }
+    }
+  };
+
+  useEffect(() => {
+    checkTokenValidity();
+  }, []);
+
+  const isAuth = Boolean(token);
 
   return (
     <div className="app">
@@ -25,46 +56,16 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route 
-              path="/" 
-              element={<LoginPage />} 
-              />
-            <Route 
-              path="/login"  
-              element={<LoginPage />} 
-              />
-            <Route 
-              path="/register" 
-              element={<RegisterPage />} 
-              />
-            <Route 
-              path="/home" 
-              element={isAuth ? <HomePage /> : <Navigate to="/" />} 
-              />
-            <Route 
-              path="/profile/:userId" 
-              element={isAuth ? <ProfilePage /> : <Navigate to="/" />} 
-            />
-            <Route 
-              path="/about"
-              element={isAuth ? <AboutPage />: <Navigate to="/" />} 
-            />
-            <Route 
-              path="/forgot-password" 
-              element={<ForgotPassword />} 
-              />
-            <Route 
-              path="/reset-password" 
-              element={<ResetPassword />} 
-              />
-            <Route 
-              path="/verify-email" 
-              element={<VerifyEmail  />} 
-              />
-            <Route 
-              path="/terms-and-conditions" 
-              element={<TermsAndConditions  />} 
-              />
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" />} />
+            <Route path="/profile/:userId" element={isAuth ? <ProfilePage /> : <Navigate to="/" />} />
+            <Route path="/about" element={isAuth ? <AboutPage /> : <Navigate to="/" />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/verify-email" element={<VerifyEmail />} />
+            <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
