6d5d92225fa6f7f87a47f9d00b30e073722e3969
maxim2100 <maxsim2100@gmail.com>
2024-08-14 17:44:38 +0300

    fix change password

--- frontend/src/scenes/profilePage/ChangePassword.jsx
+++ frontend/src/scenes/profilePage/ChangePassword.jsx
@@ -2,23 +2,72 @@ import React, { useState } from 'react';
 import { Dialog, DialogActions, DialogContent, DialogTitle, TextField, Button } from '@mui/material';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import * as yup from 'yup';
+
+const invalidChars = /[^A-Za-z\d!@#$%^&*]/g;
 
 const ChangePasswordDialog = ({ open, onClose, onChangePassword, oldPassword, setOldPassword,
 newPassword, setNewPassword, confirmNewPassword, setConfirmNewPassword }) => {
 
-  const handleChangePassword = () => {
-    if (newPassword !== confirmNewPassword) {
-      toast.error("New passwords don't match!", {
-        position: 'top-right',
+  const passwordSchema = yup.string()
+  .matches(/[A-Z]/, "At least one uppercase letter is required")
+  .matches(/[a-z]/, "At least one lowercase letter is required")
+  .matches(/\d/, "At least one number is required")
+  .min(8, "At least 8 characters are required")
+  .max(50, "Maximum 50 characters are allowed")
+  .test(
+    "invalid-characters",
+    "Password must meet the requirements and cannot contain invalid characters: ${invalidChars}",
+    function(value) {
+      if (!value) return true; // Skip validation if value is undefined
+      const invalidCharsFound = value.match(invalidChars);
+      if (invalidCharsFound) {
+        return this.createError({ message: `Password contains invalid characters: ${invalidCharsFound.join('')}` });
+      }
+      return true;
+    }
+  )
+  .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d!@#$%^&*]{8,50}$/, "Password must meet the requirements:\nAt least one uppercase letter (A-Z).\nAt least one lowercase letter (a-z).\nAt least one number (0-9).\ncan have a special character !@#$%^&*")
+  .required("Required");
+
+  const handleChangePassword = async () => {
+    try {
+      await passwordSchema.validate(newPassword);
+      if (newPassword !== oldPassword) {
+        toast.error("The new password is already in use, please choose a different password!", {
+          position: 'top-center',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
+        return;
+      }
+
+      if (newPassword !== confirmNewPassword) {
+        toast.error("New passwords don't match!", {
+          position: 'top-center',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
+        return;
+      }
+
+      onChangePassword(oldPassword, newPassword);
+    } catch (err) {
+      toast.error(err.message, {
+        position: 'top-center',
         autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
       });
-      return;
     }
-    onChangePassword(oldPassword, newPassword);
   };
 
   return (
@@ -43,7 +92,7 @@ newPassword, setNewPassword, confirmNewPassword, setConfirmNewPassword }) => {
           fullWidth
           variant="outlined"
           value={newPassword}
-          onChange={(password) => setNewPassword(password.target.value)}
+          onChange={(e) => setNewPassword(e.target.value)}
           sx={{ mb: '1rem' }}
         />
         <TextField
@@ -53,7 +102,7 @@ newPassword, setNewPassword, confirmNewPassword, setConfirmNewPassword }) => {
           fullWidth
           variant="outlined"
           value={confirmNewPassword}
-          onChange={(password) => setConfirmNewPassword(password.target.value)}
+          onChange={(e) => setConfirmNewPassword(e.target.value)}
           sx={{ mb: '1rem' }}
         />
       </DialogContent>
